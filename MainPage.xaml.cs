35b30af3804d1e94b9d98be408382b50166048bf
Lucas Apablaza <155201776+LucasApaCode@users.noreply.github.com>
2025-04-28 17:42:31 -0400

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,47 @@
-﻿namespace appMovil;
+using Microsoft.Maui.Controls;
+using System;
+
+namespace appMovil;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		UpdateColor();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+            UpdateColor();
+	}
+
+	private void UpdateColor()
 	{
-		count++;
+        int r = (int)RedSlider.Value;
+        int g = (int)GreenSlider.Value;
+        int b = (int)BlueSlider.Value;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		var color = Color.FromRgb(r, g, b);
+		ColorPreview.Color = color;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+        HexLabel.Text = $"#{r:X2}{g:X2}{b:X2}";
 	}
-}
 
+	private void OnRandomColorClicked(object sender, EventArgs e)
+    {
+        Random random = new Random();
+        RedSlider.Value = random.Next(0, 256);
+        GreenSlider.Value = random.Next(0, 256);
+        BlueSlider.Value = random.Next(0, 256);
+        UpdateColor();
+    }
+
+	private async void OnCopyColorClicked(object sender, EventArgs e)
+    {
+        string hexCode = HexLabel.Text;
+        await Clipboard.SetTextAsync(hexCode);
+        await DisplayAlert("Copiado", $"Color {hexCode} copiado al portapapeles.", "OK");
+    }
+}
