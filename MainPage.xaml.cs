94862bc6c0bcdec9acd3db348fa958166b18d18d
Lucas Apablaza <155201776+LucasApaCode@users.noreply.github.com>
2025-05-02 20:07:48 -0400

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,56 @@
-﻿namespace appMovil;
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace appMovil;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		UpdateColor();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+            UpdateColor();
+	}
+
+	private void UpdateColor()
 	{
-		count++;
+        int r = (int)RedSlider.Value;
+        int g = (int)GreenSlider.Value;
+        int b = (int)BlueSlider.Value;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		var color = Color.FromRgb(r, g, b);
+		ColorPreview.Color = color;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+        HexLabel.Text = $"#{r:X2}{g:X2}{b:X2}";
 	}
+
+	private void OnRandomColorClicked(object sender, EventArgs e)
+    {
+        Random random = new Random();
+		int red = random.Next(0, 256);
+		int green = random.Next(0, 256);
+		int blue = random.Next(0, 256);
+
+		RedSlider.Value = red;
+		GreenSlider.Value = green;
+		BlueSlider.Value = blue;
+
+		this.BackgroundColor = Color.FromRgb(red, green, blue);
+
+        UpdateColor();
+
+    }
+
+	private async void OnCopyColorClicked(object sender, EventArgs e)
+    {
+        string hexCode = HexLabel.Text;
+        await Clipboard.SetTextAsync(hexCode);
+        await DisplayAlert("Color copiado", $"Codigo de color {hexCode} copiado!", "Ok");
+    }
 }
 
